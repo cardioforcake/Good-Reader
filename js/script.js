0e8316295acacc7d3e12b0e669f012abae606836
cardioforcake <chaos_241@hotmail.com>
2021-12-29 00:29:05 -0800

    add about page

--- js/script.js
+++ js/script.js
@@ -1,56 +1,66 @@
 // "OH, GOOD READING TO YOU,"
 
 setTimeout(function(){
-  document.getElementById("goodreading").classList.remove("hidden")
-},900)
+  document.getElementById("goodreading").classList.remove("hidden");
+},900);
 setTimeout(function(){
-  document.getElementById("goodreading").classList.add("reveal")
-},1000)
+  document.getElementById("goodreading").classList.add("reveal");
+},1000);
 
 setTimeout(function(){
-  document.getElementById("goodreading").classList.add("hide")
-},3000)
+  document.getElementById("goodreading").classList.add("hide");
+},3000);
 setTimeout(function(){
-  document.getElementById("goodreading").classList.add("hidden")
-},4000)
+  document.getElementById("goodreading").classList.add("hidden");
+},4000);
 
 // "OH, SMILE"
 
 setTimeout(function(){
-  document.getElementById("smile").classList.remove("hidden")
-},4400)
+  document.getElementById("smile").classList.remove("hidden");
+},4400);
 setTimeout(function(){
-  document.getElementById("smile").classList.add("reveal")
-},4500)
+  document.getElementById("smile").classList.add("reveal");
+},4500);
 
 setTimeout(function(){
-  document.getElementById("smile").classList.add("hide")
-},6500)
+  document.getElementById("smile").classList.add("hide");
+},6500);
 setTimeout(function(){
-  document.getElementById("smile").classList.add("hidden")
-},7500)
+  document.getElementById("smile").classList.add("hidden");
+},7500);
 
 // "ARE YOU A GOOD READER?"
 
 setTimeout(function(){
-  document.getElementById("areyou").classList.remove("hidden")
-}, 7900)
+  document.getElementById("areyou").classList.remove("hidden");
+}, 7900);
 
 setTimeout(function(){
-  document.getElementById("areyou").classList.add("reveal")
-}, 8000)
+  document.getElementById("areyou").classList.add("reveal");
+}, 8000);
 
 setTimeout(function(){
-  document.querySelector(".no").classList.remove("hidden")
-}, 8900)
+  document.querySelector(".no").classList.remove("hidden");
+}, 8900);
 setTimeout(function(){
-  document.querySelector(".no").classList.add("fastReveal")
-}, 9000)
+  document.querySelector(".no").classList.add("fastReveal");
+}, 9000);
 
 setTimeout(function(){
-  document.querySelector(".yes").classList.remove("hidden")
-}, 9900)
+  document.querySelector(".yes").classList.remove("hidden");
+}, 9900);
 
 setTimeout(function(){
-  document.querySelector(".yes").classList.add("fastReveal")
-}, 10000)
+  document.querySelector(".yes").classList.add("fastReveal");
+}, 10000);
+
+document.querySelector(".homeBtn").addEventListener("click", function(){
+  document.querySelector(".home").classList.remove("hidden");
+  document.querySelector(".about").classList.add("hidden");
+});
+
+document.querySelector(".aboutBtn").addEventListener("click", function(){
+  document.querySelector(".home").classList.add("hidden");
+  document.querySelector(".about").classList.remove("hidden");
+});
